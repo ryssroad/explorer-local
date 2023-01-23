2f2d3c03075e1a5b1190fee46510e9fccc1b486c
ryssroad <ryssroad@gmail.com>
2023-01-24 03:12:50 +0600

    Initial commit

---
+++ src/navigation/vertical/index.js
@@ -0,0 +1,58 @@
+import store from '@/store'
+import { isTestnet } from '../../libs/utils'
+
+function processMenu() {
+  const chainMenus = []
+  const blockchains = []
+  Object.keys(store.state.chains.config).forEach(chain => {
+    const menu = {
+      title: chain,
+      logo: store.state.chains.config[chain].logo,
+      route: { name: 'dashboard', params: { chain } },
+    }
+    blockchains.push(menu)
+  })
+
+  if (blockchains.length > 1) {
+    chainMenus.push({ header: 'ecosystem' })
+    chainMenus.push({
+      title: 'blockchains',
+      children: blockchains,
+      tag: `${blockchains.length}`,
+      icon: 'https://ping.pub/logo.svg',
+    })
+  }
+  chainMenus.push({ header: 'LINKS' })
+  if (isTestnet()) {
+    chainMenus.push({
+      title: 'Mainnet Explorer',
+      href: 'https://ping.pub',
+      icon: 'ChromeIcon',
+    })
+  } else {
+    chainMenus.push({
+      title: 'Testnet Explorer',
+      href: 'http://testnet.ping.pub',
+      icon: 'LifeBuoyIcon',
+    })
+  }
+  chainMenus.push({
+    title: 'Twitter',
+    href: 'https://twitter.com/ping_pub',
+    icon: 'TwitterIcon',
+  })
+  chainMenus.push({
+    title: 'FAQ',
+    href: 'https://github.com/ping-pub/explorer/discussions',
+    icon: 'MessageSquareIcon',
+  })
+  chainMenus.push({
+    title: 'Github',
+    href: 'https://github.com/ping-pub/explorer',
+    icon: 'GithubIcon',
+  })
+
+  return chainMenus
+}
+
+export default processMenu()
